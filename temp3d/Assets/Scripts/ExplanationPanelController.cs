48179ae324610ba6204a63adfd8c58148b245e9a
Dhruv Chadha <dhruvchadha2212@hotmail.com>
2020-08-24 00:41:21 +0530

    Added Explanation Panel

---
+++ temp3d/Assets/Scripts/ExplanationPanelController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ExplanationPanelController : MonoBehaviour
+{
+    [SerializeField] private GameObject resumeButton;
+    [SerializeField] private GameObject flashCard;
+    [SerializeField] private GameObject imagePanel;
+    [SerializeField] private Text explanationText;
+}
