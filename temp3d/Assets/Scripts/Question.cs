a099fe3ac1bd7dc0fb81b1bdbc7834764562f384
Dhruv Chadha <dhruvchadha2212@hotmail.com>
2020-10-20 18:06:12 +0530

    minor changes

--- temp3d/Assets/Scripts/Question.cs
+++ temp3d/Assets/Scripts/Question.cs
@@ -5,13 +5,15 @@ using UnityEngine.UI;
 public class Question
 {
     public QuestionType QuestionType { get; set; }
+
     public AudioClip QuestionAudio { get; set; }
     public string QuestionString { get; set; }
     public string ExplanationString { get; set; }
     public Image QuestionImage { get; set; }
+
     public string[] Options { get; set; }
     public int CorrectOptionNumber { get; set; }
     public string[] OptionTips { get; set; }
-    public bool IsPausable { get; set; }
+
     public bool HasBeenAnsweredCorrectly { get; set; }
 }
