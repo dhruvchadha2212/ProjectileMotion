f6c8a7c6c3c5968acd6876199de892240e8cb1f8
Dhruv Chadha <dhruvchadha2212@hotmail.com>
2020-12-19 19:26:42 +0530

    Minor changes

--- temp3d/Assets/Scripts/Director.cs
+++ temp3d/Assets/Scripts/Director.cs
@@ -16,20 +16,21 @@ public class Director : MonoBehaviour
 
     private IEnumerator StartGameNarrative()
     {
+        yield return new WaitForSeconds(0f);
         //      TODO replace with new menu scene
         //yield return StartCoroutine(AudioManager.PlayAndWaitFor(Dialogues.GetQuestion(QuestionName.INTRODUCTION).audio));
         //      TODO launch the ball a couple of times to get comfortable, pause
-        yield return StartCoroutine(AskQuestion(QuestionName.WHAT_TRAJECTORY));
+        //yield return StartCoroutine(AskQuestion(QuestionName.WHAT_TRAJECTORY));
         //yield return StartCoroutine(AskQuestion(QuestionName.WHY_CURVED));
         //yield return StartCoroutine(AskQuestion(QuestionName.HOW_MANY_AXES));
         //      TODO good job ! you are an intuitive person.
-        //yield return StartCoroutine(questManager.BeginQuest(QuestName.VISUALISE_VERTICAL_COMPONENT_OF_VELOCITY));
+        //yield return StartCoroutine(BeginQuest(QuestName.VISUALISE_VERTICAL_COMPONENT_OF_VELOCITY));
         //yield return StartCoroutine(AskQuestion(QuestionName.VERTICAL_IS_INDEPENDENT));
         //yield return StartCoroutine(AskQuestion(QuestionName.VERTICAL_COMPONENT_FORMULA));
         //yield return StartCoroutine(AskQuestion(QuestionName.TIME_OF_FLIGHT_CAN_BE_FOUND));
         //yield return StartCoroutine(AskQuestion(QuestionName.WHICH_EQUATION_TIME_OF_FLIGHT));
         //      TODO Quest 1 completed ! (use enum QUEST_1_COMPLETED). Show time of flight formula card
-        yield return StartCoroutine(questManager.BeginQuest(QuestName.VISUALISE_HORIZONTAL_COMPONENT_OF_VELOCITY));
+        //yield return StartCoroutine(BeginQuest(QuestName.VISUALISE_HORIZONTAL_COMPONENT_OF_VELOCITY));
         //yield return StartCoroutine(AskQuestion(QuestionName.ANY_HORIZONTAL_FORCE));
         //yield return StartCoroutine(AskQuestion(QuestionName.IS_HORIZONTAL_VELOCITY_AFFECTED));
         //yield return StartCoroutine(AskQuestion(QuestionName.HORIZONTAL_COMPONENT_FORMULA));
@@ -47,5 +48,10 @@ public class Director : MonoBehaviour
         yield return new WaitUntil(() => GameState.currentQuestionHasBeenAnsweredCorrectly);
         //yield return StartCoroutine(audioManager.PlayAndWaitFor(dialoguesOld.GetRandomAppreciation()));
         yield return new WaitForSeconds(0.5f);
+    }
+
+    private IEnumerator BeginQuest(QuestName questName)
+    {
+        yield return StartCoroutine(questManager.BeginQuest(questName));
     }
 }
--- temp3d/Assets/Scripts/Quest.cs
+++ temp3d/Assets/Scripts/Quest.cs
@@ -7,5 +7,3 @@
         this.tasks = tasks;
     }
 }
-
-
