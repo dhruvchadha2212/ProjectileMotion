dd5498d7e847370587cb2f764a0374f5ab4ba9e3
Dhruv Chadha <dhruvchadha2212@hotmail.com>
2020-12-31 22:51:53 +0530

    UAT changes part 2

--- temp3d/Assets/Scripts/Quiz System/Director.cs
+++ temp3d/Assets/Scripts/Quiz System/Director.cs
@@ -17,21 +17,21 @@ public class Director : MonoBehaviour
     private IEnumerator StartGameNarrative()
     {
         //      TODO replace with new menu scene
-        //yield return StartCoroutine(AudioManager.PlayAndWaitFor(Dialogues.GetQuestion(QuestionName.INTRODUCTION).audio));
+        yield return StartCoroutine(AudioManager.PlayAndWaitFor(Dialogues.GetQuestion(QuestionName.INTRODUCTION).audio));
         //      TODO solve the "question without panel/audio only" issue
-        //yield return new WaitForSeconds(2f);
-        //yield return StartCoroutine(TaskManager.WaitForTaskCompletion(new BallLaunchTask()));
-        //yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(2f);
+        yield return StartCoroutine(TaskManager.WaitForTaskCompletion(new BallLaunchTask()));
+        yield return new WaitForSeconds(2f);
         ////      TODO launch the ball a couple of times to get comfortable, pause
-        //yield return StartCoroutine(AskQuestion(QuestionName.WHAT_TRAJECTORY));
-        //yield return StartCoroutine(AskQuestion(QuestionName.WHY_CURVED));
-        //yield return StartCoroutine(AskQuestion(QuestionName.HOW_MANY_AXES));
+        yield return StartCoroutine(AskQuestion(QuestionName.WHAT_TRAJECTORY));
+        yield return StartCoroutine(AskQuestion(QuestionName.WHY_CURVED));
+        yield return StartCoroutine(AskQuestion(QuestionName.HOW_MANY_AXES));
         //      TODO good job ! you are an intuitive person.
-        //yield return StartCoroutine(BeginQuest(QuestName.VISUALISE_VERTICAL_COMPONENT_OF_VELOCITY));
-        //yield return StartCoroutine(AskQuestion(QuestionName.VERTICAL_IS_INDEPENDENT));
-        //yield return StartCoroutine(AskQuestion(QuestionName.VERTICAL_COMPONENT_FORMULA));
-        //yield return StartCoroutine(AskQuestion(QuestionName.TIME_OF_FLIGHT_CAN_BE_FOUND));
-        //yield return StartCoroutine(AskQuestion(QuestionName.WHICH_EQUATION_TIME_OF_FLIGHT));
+        yield return StartCoroutine(BeginQuest(QuestName.VISUALISE_VERTICAL_COMPONENT_OF_VELOCITY));
+        yield return StartCoroutine(AskQuestion(QuestionName.VERTICAL_IS_INDEPENDENT));
+        yield return StartCoroutine(AskQuestion(QuestionName.VERTICAL_COMPONENT_FORMULA));
+        yield return StartCoroutine(AskQuestion(QuestionName.TIME_OF_FLIGHT_CAN_BE_FOUND));
+        yield return StartCoroutine(AskQuestion(QuestionName.WHICH_EQUATION_TIME_OF_FLIGHT));
         //      TODO Quest 1 completed ! (use enum QUEST_1_COMPLETED). Show time of flight formula card
         yield return StartCoroutine(BeginQuest(QuestName.VISUALISE_HORIZONTAL_COMPONENT_OF_VELOCITY));
         yield return StartCoroutine(AskQuestion(QuestionName.ANY_HORIZONTAL_FORCE));
